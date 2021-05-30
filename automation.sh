5ae13abb9fe544f863a928e3b27639323a947ab6
namita-shettygiri <namitashettygiri@gmail.com>
2021-05-30 13:47:24 +1000

    Changes done to automation.sh required by Task 3

--- automation.sh
+++ automation.sh
@@ -29,3 +29,24 @@ myname="Namita"
 s3_bucket="upgrad-namita"
 tar -cvf /tmp/"$myname-httpd-logs-$timestamp".tar /var/log/apache2/*.log
 aws s3 cp /tmp/${myname}-httpd-logs-${timestamp}.tar s3://${s3_bucket}
+
+#Store the tar filesize in a variable and covert to kilobytes
+filesize=$(stat --printf="%s" /tmp/"$myname-httpd-logs-$timestamp".tar)
+filesize_KB=`expr $filesize / 1000`
+
+#If inventory.html doesn't exist create the file with header and append the required values, else just append the values
+if [[ ! -f /var/www/html/inventory.html ]]
+then
+	cd /var/www/html/
+	touch inventory.html
+	echo LogType$'\t\t'Time Created$'\t\t'Type$'\t\t'Size >> inventory.html
+	echo httpd-logs$'\t\t'$timestamp$'\t\t'tar$'\t\t'$filesize >> inventory.html
+else
+	cd /var/www/html/
+	echo httpd-logs$'\t\t'$timestamp$'\t\t'tar$'\t\t'$filesize_KB"K" >> inventory.html
+fi
+
+if [[ ! -f /etc/cron.d/automation ]]
+then
+	echo "0 0 * * * root /root/Automation_Project/automation.sh" >> /etc/cron.d/automation
+fi
